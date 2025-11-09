d57d8031b119470e756233e20ddfa2e8b1241639
Arjunhabibi870 <136176877+Arjunhabibi870@users.noreply.github.com>
2025-11-09 20:44:47 +0700

    Add favicon link to app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name', 'SICETAR') }} - @yield('title')</title>
+    <link rel="icon" href="{{ asset('img/Bpjslogo.jpg') }}" type="image/jpg">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
