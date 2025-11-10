e2dfccf8dea3a7499fb83c1e6687383abe0a0c50
Arjunhabibi870 <136176877+Arjunhabibi870@users.noreply.github.com>
2025-11-10 09:20:00 +0700

    Add favicon link to login page

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <title>Login SICETAR</title>
-
+  <link rel="icon" href="{{ asset('img/Bpjslogo.jpg') }}" type="image/jpg">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
   <style>
